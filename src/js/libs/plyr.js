74542cffd9e0b8e1a68cf5da99a8a5465be47601
Pasha <pashahrytsyna@gmail.com>
2025-06-07 00:46:12 +0200

    feat: start adding installation page

--- src/js/libs/plyr.js
+++ src/js/libs/plyr.js
@@ -1,8 +1,9 @@
 export const plyrInit = (PlyrInstance) => {
-  const playerElement = document.querySelector("#about-player");
+  const aboutPlayerElement = document.querySelector("#about-player");
+  const installationPlayerElement = document.querySelector('#installation-player')
 
-  if (playerElement) {
-    const player = new PlyrInstance(playerElement, {
+  if (aboutPlayerElement) {
+    const player = new PlyrInstance(aboutPlayerElement, {
       clickToPlay: true,
       muted: true,
       resetOnEnd: true,
@@ -28,4 +29,34 @@ export const plyrInit = (PlyrInstance) => {
       console.warn("Plyr target element #about-player not found.");
     }
   }
+
+  if(installationPlayerElement) {
+    const player = new PlyrInstance(installationPlayerElement, {
+      clickToPlay: true,
+      muted: true,
+      resetOnEnd: true,
+      // controls: ['mute'],
+    });
+
+    if ("IntersectionObserver" in window) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.intersectionRatio < 0.5 && !player.paused) {
+              player.pause();
+            }
+            if (entry.intersectionRatio > 0.5 && player.paused) {
+              player.play();
+            }
+          });
+        },
+        { threshold: 0.5 },
+      );
+      observer.observe(player.elements.container);
+    } else {
+      console.warn("Plyr target element #about-player not found.");
+    }
+  }
+
+
 };
